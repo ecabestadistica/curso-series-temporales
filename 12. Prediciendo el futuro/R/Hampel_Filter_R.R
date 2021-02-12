51eed908a5d2a2e0bab5bac208065f6154411861
Elisa Cabana <elisacabana@MacBook-Pro-de-Elisa.local>
2021-02-12 20:42:22 +0100

    Agregando material

---
+++ 12. Prediciendo el futuro/R/Hampel_Filter_R.R
@@ -0,0 +1,39 @@
+install.packages("pracma")
+library(pracma)
+
+set.seed(33)
+
+
+# Generar los datos
+x <- numeric(1024)
+z <- rnorm(1024)
+x[1] <- z[1]
+for (i in 2:1024) {
+  x[i] <- 0.4*x[i-1] + 0.8*x[i-1]*z[i-1] + z[i]
+}
+
+
+# Aplicar el Filtro de Hampel
+omad <- hampel(x, k=20)
+
+
+# Indice de los atípicos detectados:
+omad$ind
+
+
+# Gráfico detectando atípicos
+plot(1:1024, x, type="l")
+points(omad$ind, x[omad$ind], pch=21, col="darkred")
+
+
+# Nueva serie sin atípicos
+x_new=x
+x_new[omad$ind]=median(x)
+
+
+# Ambas series
+plot(1:1024, x, type="l",col="red",xlim=c(0, 1050), ylim=c(-20,30))
+par(new=TRUE)
+plot(1:1024, x_new, type="l",col="blue",xlim=c(0, 1050), ylim=c(-20,30),axes= FALSE, xlab='', ylab='' )
+  
+  
